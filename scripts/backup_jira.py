bbe1f741ddc2dfb14d79493271d56adde4461694
Sergi Saavedra <saavedra.sergi@gmail.com>
2026-05-04 09:56:29 +0200

    Update backup_jira.py

--- scripts/backup_jira.py
+++ scripts/backup_jira.py
@@ -9,4 +9,10 @@ response = requests.post(
     auth=auth
 )
 
-print(response.json())
+# Esto imprimirá el código (401, 403, 400, etc.) y el texto del error
+print(f"Status Code: {response.status_code}")
+print(f"Respuesta de Jira: {response.text}")
+
+# Esto forzará al script a fallar si hay un error, pero habiendo impreso lo de arriba
+response.raise_for_status()
+
