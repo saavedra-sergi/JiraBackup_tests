1d20975d0aba6cdd5263469d107a78a1ad0d2b00
Sergi Saavedra <saavedra.sergi@gmail.com>
2026-05-04 11:40:54 +0200

    Update backup_jira.py

--- scripts/backup_jira.py
+++ scripts/backup_jira.py
@@ -40,11 +40,11 @@ def run_backup():
 
     endpoint_run = f"{base_url}/rest/backup/1/export/runbackup"
     payload = {
-        "cbAttachments": "true",
-        "exportToCloud": "true",
-        "cbAvatars": "true",
-        "cbCustomFields": "true",
-        "cbWorklogs": "true"
+        "cbAttachments": True,
+        "exportToCloud": True,
+        "cbAvatars": True,
+        "cbCustomFields": True,
+        "cbWorklogs": True
     }
 
     print("🚀 Iniciando proceso de backup profesional...")
