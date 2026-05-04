b7c0893ce2755e5e871c62ebd02731339278a11e
Sergi Saavedra <saavedra.sergi@gmail.com>
2026-05-04 09:53:18 +0200

    Update backup_jira.py

--- scripts/backup_jira.py
+++ scripts/backup_jira.py
@@ -1,1 +1,12 @@
+payload = {
+    "cbAttachments": "true", # ¿Quieres incluir archivos adjuntos?
+    "exportToCloud": "true"
+}
 
+response = requests.post(
+    f"{JIRA_URL}/rest/backup/1/export/runbackup",
+    json=payload,
+    auth=auth
+)
+
+print(response.json())
